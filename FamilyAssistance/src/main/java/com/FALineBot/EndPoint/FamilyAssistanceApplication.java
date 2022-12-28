3fe97d24c2603fbac5c24394e34d9182c7bcc3af
chsu23 <chsu23@US-5CG01050N1>
2022-12-28 14:20:06 +0800

    將啟動類新增Trycatch

--- FamilyAssistance/src/main/java/com/FALineBot/EndPoint/FamilyAssistanceApplication.java
+++ FamilyAssistance/src/main/java/com/FALineBot/EndPoint/FamilyAssistanceApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FamilyAssistanceApplication {
 
 	public static void main(String[] args) {
+		try {
 		SpringApplication.run(FamilyAssistanceApplication.class, args);
+		}catch(Throwable e) {
+			e.printStackTrace();
+		}
 	}
 
 }
