50c1226dc98ef624826bf3d4892f2cd003dbbd2e
chsu23 <chsu23@US-5CG01050N1>
2023-01-16 12:00:49 +0800

    新增Component

--- FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Service/Impl/ReplyMessageServiceImpl.java
+++ FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Service/Impl/ReplyMessageServiceImpl.java
@@ -1,21 +1,25 @@
 package com.FALineBot.EndPoint.Service.Impl;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import com.FALineBot.EndPoint.Service.ReplyMessageService;
 
+@Component
 public class ReplyMessageServiceImpl implements ReplyMessageService{
-	
+
+	@Autowired
 	private RestTemplate restTemplate = new RestTemplate();
 	private String Reply_Url = "https://api.line.me/v2/bot/message/reply";
 	private String LINE_SECRET = "/SG/if6TI6qOaEqeniCsdaX4Y/R7pzzjw6mhQrwsCCQK0aItavD/9jhH/OwsAlDbNAlWcGJU2W5hn9hK2WrkU1kk0bM77KdRfIVcop96uIJurFGCpGMEiNoGlmjo59dGdMNOSUBi8AhTwwTyDHeCuAdB04t89/1O/w1cDnyilFU=";
 
-	
+	@Override
 	public void ReplyTextMessage(String Message,String token) {
 		//建立回傳訊息標頭
 		HttpHeaders headers = new HttpHeaders();
