1adb274c6eb1c7e322e04254b3f3bc24faed4627
chsu23 <chsu23@US-5CG01050N1>
2023-03-10 12:22:55 +0800

    更新刪除願望功能

--- FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Controller/MainController.java
+++ FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Controller/MainController.java
@@ -51,12 +51,14 @@ public class MainController {
 	//ＬineBot主功能
 	@SuppressWarnings("rawtypes")
 	@PostMapping("/messaging")
+	
 	public ResponseEntity messagingAPI(@RequestHeader("X-Line-Signature") String X_Line_Signature,
 			@RequestBody String requestBody) {
 		//取得Request訊息
 		JSONObject object = new JSONObject(requestBody);	
 		JSONObject event = new JSONObject();
 		
+		//處理Message訊息
 		for(int i=0; i<object.getJSONArray("events").length(); i++) {
 			//判斷陣列裡是否有續鐔
 			 if(object.getJSONArray("events").getJSONObject(i).getString("type").equals("message")) {
@@ -65,6 +67,9 @@ public class MainController {
 				 event = object.getJSONArray("events").getJSONObject(i);
 				 String Message =event.getJSONObject("message").getString("text").toString();
 				 String wisher = event.getJSONObject("source").getString("userId").toString();
+				 String K1 = "";
+				 String K2 = "";
+				 
 				 //關鍵字搜尋功能
 				 //新增願望清單功能
 				 if(Message.indexOf("新增願望")!=-1) {
@@ -75,9 +80,11 @@ public class MainController {
 				            System.out.println(newStr[k]);
 				            if(k == 1) {
 				            wishListParam.setPersent_name(newStr[k]);
+				            //K1 = newStr[k];
 				            }
 				            if(k == 2) {
 				            wishListParam.setRemark(newStr[k]);
+				            //K2 = newStr[k];
 				            }
 
 				        }
@@ -85,6 +92,9 @@ public class MainController {
 				        wishListParam.setWisher(wisher);
 			            wishListService.createProduct(wishListParam);
 			            replyMessageService.ReplyTextMessage("新增願望："+wishListParam.getPersent_name(),token);
+			            //System.out.println("K1: "+K1);
+			            //System.out.println("K2: "+K2);
+			            
 			            return new ResponseEntity<String>("OK", HttpStatus.OK);
 					 
 				 }
@@ -100,6 +110,7 @@ public class MainController {
 						}
 					}
 			       replyMessageService.ReplyTextMessage("願望清單：\n" + test,token);
+			       System.out.println("願望清單"+ test);
 			       return new ResponseEntity<String>("OK", HttpStatus.OK);
 					 
 				 }
@@ -125,13 +136,15 @@ public class MainController {
 				        Integer ID = 0;
 				        for (int k = 1; k < newStr.length; k++) {
 				            System.out.println(newStr[k]);
-				            if(k == 2) {
+				            if(k == 1) {
 				            	ID=Integer.parseInt(newStr[k]);
 				            	wishListService.deleteWishListByID(ID);
+				            	//System.out.println("我有執行刪除願望");
 				            	replyMessageService.ReplyTextMessage("刪除成功",token);
 				            }
 
 				        }
+				        return new ResponseEntity<String>("Delete OK", HttpStatus.OK);
 					 
 				 }
 				 //查詢訊息
