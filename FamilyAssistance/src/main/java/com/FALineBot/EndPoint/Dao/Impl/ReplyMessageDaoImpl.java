9766bba88bf36159b8310462ada122aa9c25d1b6
chsu23 <chsu23@US-5CG01050N1>
2023-03-16 11:33:46 +0800

    Check Flex Output

--- FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Dao/Impl/ReplyMessageDaoImpl.java
+++ FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Dao/Impl/ReplyMessageDaoImpl.java
@@ -104,8 +104,9 @@ public class ReplyMessageDaoImpl implements ReplyMessageDao{
 		Messages.put(FlexMessage);
 		PayloadContent.put("messages",Messages); 
         //回傳訊息
-        HttpEntity<String> entity = new HttpEntity<String>(PayloadContent.toString(), headers);
-        restTemplate.exchange(Reply_Url,HttpMethod.POST, entity, String.class);
+		System.out.println(PayloadContent.toString());
+        //HttpEntity<String> entity = new HttpEntity<String>(PayloadContent.toString(), headers);
+        //restTemplate.exchange(Reply_Url,HttpMethod.POST, entity, String.class);
 		
 	}
 	
