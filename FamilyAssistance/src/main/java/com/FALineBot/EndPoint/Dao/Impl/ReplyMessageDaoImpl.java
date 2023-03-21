8537147da1d4f5d50392c88478915b8cb05cc0e8
chsu23 <chsu23@US-5CG01050N1>
2023-03-21 15:05:21 +0800

    看看塞完資料長怎樣

--- FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Dao/Impl/ReplyMessageDaoImpl.java
+++ FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Dao/Impl/ReplyMessageDaoImpl.java
@@ -130,6 +130,8 @@ public class ReplyMessageDaoImpl implements ReplyMessageDao{
 							FMBodyContents_SingleWishDataADataArray.put(FMBodyContents_SingleWishDataADataAB);
 							FMBodyContents_SingleWishDataAData.put("contents", FMBodyContents_SingleWishDataADataArray);
 							FMBodyContents_SingleWishDataArray.put(FMBodyContents_SingleWishDataAData);
+							System.out.println("塞完資料");
+							System.out.println("資料名稱"+FMBodyContents_SingleWishDataArray.toString());
 				}else {
 
 				}
