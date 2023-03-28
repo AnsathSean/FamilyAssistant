bdd86030753dbfb50c377661d674d10aae9ac179
chsu23 <chsu23@US-5CG01050N1>
2023-03-28 12:29:30 +0800

    將資料變成取所有願望

--- FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Controller/WebController.java
+++ FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Controller/WebController.java
@@ -22,18 +22,18 @@ public class WebController {
 	@GetMapping("/MyWishlist/{wisher}")
     public String MyWishList(@PathVariable String wisher,Model model) {
     	List<WishList> list = wishListService.findAllWishList();
-    	List<WishList> MyWishList = new ArrayList<WishList>();
-    	Integer order = 0;
-    	for (WishList e : list) {
+    	//List<WishList> MyWishList = new ArrayList<WishList>();
+    	//Integer order = 0;
+    	//for (WishList e : list) {
     		
-    		if(e.wisher.equals(wisher)) {
-				order = order+1;
-				e.order = order;
-				MyWishList.add(order,e);
-    	}
+    		//if(e.wisher.equals(wisher)) {
+				//order = order+1;
+				//e.order = order;
+				//MyWishList.add(order,e);
+    	//}
     		
-    	}
-    	model.addAttribute("wishList", MyWishList);
+    	//}
+    	model.addAttribute("wishList", list);
         return "MyWishList";
     }
 	
