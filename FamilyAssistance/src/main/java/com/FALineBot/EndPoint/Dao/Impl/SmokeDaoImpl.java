fdfba8bbeb9036a164f45b652e253259da68ff77
EricHsu <erichsu@perform-global.com>
2024-06-05 11:33:03 +0800

    再次修改回傳的時間，希望這次就是對的。

--- FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Dao/Impl/SmokeDaoImpl.java
+++ FamilyAssistance/src/main/java/com/FALineBot/EndPoint/Dao/Impl/SmokeDaoImpl.java
@@ -3,7 +3,9 @@ package com.FALineBot.EndPoint.Dao.Impl;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -35,8 +37,15 @@ public class SmokeDaoImpl implements SmokeDao{
 
 	@Override
 	public Date GetLastSmokeTime() {
+	    // 从数据库中获取 UTC 时间的 Timestamp 对象
 	    String query = "SELECT CreateTime FROM smoke ORDER BY UUID DESC LIMIT 1";
 	    Timestamp lastSmokeTime = jdbcTemplate.queryForObject(query, Timestamp.class);
-	    return new Date(lastSmokeTime.getTime());
-	}
+
+	    // 将 UTC 时间转换为指定时区的 Date 对象
+	    TimeZone timeZone = TimeZone.getTimeZone("Asia/Shanghai"); // 设置为你想要的时区
+	    Calendar calendar = Calendar.getInstance();
+	    calendar.setTimeInMillis(lastSmokeTime.getTime());
+	    calendar.setTimeZone(timeZone);
+	    return calendar.getTime();
+}
 }
